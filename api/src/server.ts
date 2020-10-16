c792fe2521e21080abb837ffde162775011b1840
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-10-16 00:02:49 -0300

    feat: Dia 2 (backend) - Configurando ORM

---
+++ api/src/server.ts
@@ -0,0 +1,14 @@
+import express, { response } from 'express';
+
+import './database/connection'
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users/:id', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
+
+/* localhost:3333 */
+app.listen(3333);
