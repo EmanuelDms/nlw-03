f35ed90c944446bfe2fef1d13d2b6386f29a6879
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-10-16 20:03:41 -0300

    feat: Dia 2 (backend) - Configurando ORM (Rotas)

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,35 @@
-import express, { response } from 'express';
-
+import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 import './database/connection'
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/users/:id', (request, response) => {
+app.post('/orphanages', (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
   return response.json({ message: 'Hello World!' });
 });
 
